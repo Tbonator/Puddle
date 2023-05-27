e9f8940dad22b622a17dd5346b69291000f34b6e
Tbonator <thabo2g5@yahoo.ca>
2023-05-27 10:11:54 +0200

    adding edit page

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
-from .forms import NewItemForm
+from .forms import NewItemForm,EditItemForm
 
 from .models import Item
 
@@ -44,3 +44,22 @@ def delete(request,pk):
     
     return redirect('dashboard:index')
 
+@login_required
+def edit(request,pk):
+    #get the item_id
+    item = get_object_or_404(Item,pk=pk,created_by = request.user)
+    
+    if request.method =="POST":
+        form = EditItemForm(request.POST,request.FILES,instance =item)
+        
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail',pk=item.id)
+    else:       
+        form = EditItemForm(instance = item)
+    
+    return render (request,'item/form.html',{
+        'form':form,
+        'title':'Edit Item'
+    })
