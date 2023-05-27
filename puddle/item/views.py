08f77173ceeb2a9f50a8758a35fc5c4c2f2f98cb
Tbonator <thabo2g5@yahoo.ca>
2023-05-27 12:58:55 +0200

    add Search and Filtering -

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,13 +1,34 @@
+
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
 from .forms import NewItemForm,EditItemForm
 
-from .models import Item
+from .models import Item,Category
 
-# Create your views here.
-
-
+def items(request):
+    query = request.GET.get('query','')
+    items = Item.objects.filter(is_sold =False)
+    categories = Category.objects.all()
+    category_id = request.GET.get('category',0)
+    
+    if category_id:
+        items = items.filter(category_id = category_id)
+    
+    if query:
+        items = items.filter(Q(name__icontains=query)|Q(description__icontains = query))
+   
+    return render (request,'item/items.html',{
+        'items': items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id)
+    })
+    
+    
+    
+    
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     #all items in category same as item 
